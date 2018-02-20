6b18556527cf81ba8efd222c6e160692b465f621
Scott Gigante <scottgigante@gmail.com>
2018-02-20 10:32:35 -0500

    Rename `src` to `python` to avoid R namespace error
    R installations in Windows assume contents of the `src` folder are C++
    code to be compiled along with the R project. This causes installation
    failure.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='magic',
       description='MAGIC',
       author='',
       author_email='',
-      package_dir={'': 'src'},
+      package_dir={'': 'python'},
       packages=['magic'],
       install_requires=[
           'numpy>=1.10.0',
@@ -27,8 +27,8 @@ setup(name='magic',
           'FCS': ['fcsparser'],
           'HDF5': ['tables'],
       },
-      scripts=['src/magic/magic_gui.py',
-               'src/magic/MAGIC.py'],
+      scripts=['python/magic/magic_gui.py',
+               'python/magic/MAGIC.py'],
       )
 
 
