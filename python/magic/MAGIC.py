b4fc8c338d40ff7ba12c752997c39efea72db650
Scott Gigante <scottgigante@gmail.com>
2018-05-28 17:50:11 -0400

    initial attempt at refactor

--- python/magic/MAGIC.py
+++ python/magic/MAGIC.py
@@ -1,120 +1,343 @@
-#!/usr/local/bin/python3
+# author: Scott Gigante <scott.gigante@yale.edu>
+# (C) 2017 Krishnaswamy Lab GPLv2
+from __future__ import print_function, division, absolute_import
 
-import os
-import sys
-import argparse
-import numpy as np 
+import numpy as np
+import graphtools
+from sklearn.base import BaseEstimator
+from sklearn.exceptions import NotFittedError
+from sklearn.decomposition import PCA
+import warnings
+import matplotlib.pyplot as plt
 
-import magic
+from .optimal_t import compute_optimal_t
+from .utils import check_int, check_positive, check_between, check_in, check_if_not
+from .logging import set_logging, log_start, log_complete, log_info, log_debug
 
-class ArgumentParserError(Exception):
-	pass
-
-class NewArgumentParser(argparse.ArgumentParser):
-	def error(self, message):
-		print(message)
-		sys.exit(0)
-
-def parse_args(args):
-	p = NewArgumentParser(description='run MAGIC')
-	p.add_argument('filetype',
-                   choices=['csv', '10x', '10x_HDF5', 'mtx'],
-                   help='what is the file type of your original data?')
-
-	a = p.add_argument_group('data loading parameters')
-	a.add_argument('-d', '--data-file', metavar='D', required=True,
-                   help='File path of input data file.')
-	a.add_argument('-o', '--output-file', metavar='O', required=True,
-				   help='File path of where to save the MAGIC imputed data (in csv format).')
-	a.add_argument('-g', '--genome', metavar='G',
-					help='Genome must be specified when loading 10x_HDF5 data.')
-	a.add_argument('--gene-name-file', metavar='GN', 
-					help='Gene name file must be specified when loading mtx data.')
-	a.add_argument('--use-ensemble-ids', default=False, action='store_true',
-					help='Use ensemble IDs instead of gene names.')
-	a.add_argument('--cell-axis', metavar='CA', default='rows',
-					choices=['rows', 'columns'], 
-					help='When loading a csv, specify whether cells are on rows or columns (Default = \'rows\').')
-	a.add_argument('--skip-rows', default=0, type=int,
-					help='When loading a csv, number of rows to skip after the header row (Default = 0).')
-	a.add_argument('--skip-columns', default=0, type=int,
-					help='When loading a csv, number of columns to skip after the header columns (Default = 0).')
-
-	n = p.add_argument_group('normalization/filtering parameters')
-	n.add_argument('-n', '--no-normalize', default=True, action='store_false',
-					help='Do not perform library size normalization on the data')
-	n.add_argument('-l', '--log-transform', metavar='L', default=None, type=float,
-					help='Log-transform data with the specified pseudocount.')
-	n.add_argument('--mols-per-cell-min', default=0, type=int,
-					help='Minimum molecules/cell to use in filtering.')
-	n.add_argument('--mols-per-cell-max', default=np.inf, type=int,
-					help='Maximum molecules/cell to use in filtering.')
-
-	m = p.add_argument_group('MAGIC parameters')
-	m.add_argument('-p', '--pca-components', metavar='P', default=20, type=int,
-				   help='Number of pca components to use when running MAGIC (Default = 20).')
-	m.add_argument('--pca-non-random', default=True, action='store_false',
-				    help='Do not used randomized solver in PCA computation.')
-	m.add_argument('-t', metavar='T', default=None, type=int,
-					help='t parameter for running MAGIC. Default = None, in this case, the optimal t will be calculated .')
-	m.add_argument('-k', metavar='K', default=30, type=int,
-					help='Number of nearest neighbors to use when running MAGIC (Default = 30).')
-	m.add_argument('-ka', metavar='KA', default=10, type=int,
-					help='knn-autotune parameter for running MAGIC (Default = 10).')
-	m.add_argument('-e', '--epsilon', metavar='E', default=1, type=int,
-					help='Epsilon parameter for running MAGIC (Default = 1).')
-	m.add_argument('-r', '--rescale', metavar='R', default=99, type=int,
-					help='Percentile to rescale data to after running MAGIC (Default = 99).')
-	m.add_argument('--plot', default=False, action='store_true',
-					help='Plot R2 plot generated in optimal t calculation (Default=False).')
-	m.add_argument('--t-max', metavar='TM', default=12, type=int,
-					help='Maximum t value used in optimal t calculation (Default=12).')
-	m.add_argument('--n-genes', metavar='NG', default=500, type=int,
-					help='Number of genes to use in optimal t calculation, a smaller number of genes speeds up the calculation (Default=500).')
-
-	try:
-		return p.parse_args(args)
-	except ArgumentParserError:
-		raise
+try:
+    import anndata
+except ImportError:
+    # anndata not installed
+    pass
 
 
-def main(args: list = None):
-	args = parse_args(args)
+class MAGIC(BaseEstimator):
+    """MAGIC TODO [1]
 
-	try:
-		if args.filetype == 'csv':
-			scdata = magic.mg.SCData.from_csv(os.path.expanduser(args.data_file), 
-											  data_type='sc-seq', normalize=False, 
-											  cell_axis=0 if args.cell_axis=='rows' else 1,
-											  rows_after_header_to_skip=args.skip_rows,
-											  cols_after_header_to_skip=args.skip_columns)
-		elif args.filetype == 'mtx':
-			scdata = magic.mg.SCData.from_mtx(os.path.expanduser(args.data_file),
-											  os.path.expanduser(args.gene_name_file), normalize=False)
-		elif args.filetype == '10x':
-			scdata = magic.mg.SCData.from_10x(os.path.expanduser(args.data_file), normalize=False, 
-											  use_ensemble_id=args.use_ensemble_ids)
+    Parameters
+    ----------
 
-		elif args.filetype == '10x_HDF5':
-			scdata = magic.mg.SCData.from_10x_HDF5(os.path.expanduser(args.data_file), args.genome, 
-												   normalize=False, use_ensemble_id=args.use_ensemble_ids)
+    # TODO
 
-		scdata.filter_scseq_data(filter_cell_min=args.mols_per_cell_min, filter_cell_max=args.mols_per_cell_max)
+    k : int, optional, default: 15
+        number of nearest neighbors on which to build kernel
 
-		if args.no_normalize:
-			scdata = scdata.normalize_scseq_data()
+    a : int, optional, default: 10
+        sets decay rate of kernel tails.
+        If None, alpha decaying kernel is not used
 
-		if args.log_transform != None:
-			scdata.log_transform_scseq_data(pseudocount=args.log_transform)
+    t : int, optional, default: 'auto'
+        power to which the diffusion operator is powered.
+        This sets the level of diffusion. If 'auto', t is selected
+        according to the knee point in the Von Neumann Entropy of
+        the diffusion operator
 
-		scdata.run_magic(n_pca_components=args.pca_components, random_pca=args.pca_non_random, t=args.t,
-						 k=args.k, ka=args.ka, epsilon=args.epsilon, rescale_percent=args.rescale,
-						 compute_t_make_plots=args.plot, t_max=args.t_max, compute_t_n_genes=args.n_genes)
+    n_pca : int, optional, default: 100
+        Number of principal components to use for calculating
+        neighborhoods. For extremely large datasets, using
+        n_pca < 20 allows neighborhoods to be calculated in
+        roughly log(n_samples) time.
 
-		scdata.magic.to_csv(os.path.expanduser(args.output_file))
+    n_jobs : integer, optional, default: 1
+        The number of jobs to use for the computation.
+        If -1 all CPUs are used. If 1 is given, no parallel computing code is
+        used at all, which is useful for debugging.
+        For n_jobs below -1, (n_cpus + 1 + n_jobs) are used. Thus for
+        n_jobs = -2, all CPUs but one are used
 
-	except:
-		raise
+    random_state : integer or numpy.RandomState, optional, default: None
+        The generator used to initialize SMACOF (metric, nonmetric) MDS
+        If an integer is given, it fixes the seed
+        Defaults to the global `numpy` random number generator
 
-if __name__ == '__main__':
-	main(sys.argv[1:])
+    verbose : `int` or `boolean`, optional (default: 1)
+        If `True` or `> 0`, print status messages
+
+    Attributes
+    ----------
+
+    X : array-like, shape=[n_samples, n_dimensions]
+
+    X_imputed : array-like, shape=[n_samples, n_dimensions]
+
+    graph : graphtools.BaseGraph
+        The graph built on the input data
+
+    Examples
+    --------
+     # TODO
+
+    References
+    ----------
+    .. [1] TODO
+    """
+
+    def __init__(self, n_pca=100, t='auto', k=12, a=10, rescale=0,
+                 n_jobs=1, random_state=None, verbose=1):
+        self.a = a
+        self.k = k
+        self.t = t
+        self.n_pca = n_pca
+        self.n_jobs = n_jobs
+        self.random_state = random_state
+
+        self.graph = None
+        self.X = None
+        self._check_params()
+        self.verbose = verbose
+        set_logging(verbose)
+
+    @property
+    def diff_op(self):
+        """The diffusion operator calculated from the data
+        """
+        if self.graph is not None:
+            if isinstance(self.graph, graphtools.LandmarkGraph):
+                return self.graph.landmark_op
+            else:
+                return self.graph.diff_op
+        else:
+            raise NotFittedError("This MAGIC instance is not fitted yet. Call "
+                                 "'fit' with appropriate arguments before "
+                                 "using this method.")
+
+    def _check_params(self):
+        """Check MAGIC parameters
+
+        This allows us to fail early - otherwise certain unacceptable
+        parameter choices, such as t=-1, would only fail after
+        minutes of runtime.
+
+        Raises
+        ------
+        ValueError : unacceptable choice of parameters
+        """
+        check_positive(k=self.k,
+                       n_pca=self.n_pca)
+        check_int(k=self.k,
+                  n_jobs=self.n_jobs)
+        # TODO: epsilon
+        check_if_not(0, check_between, v_min=1,
+                     v_max=100, rescale=self.rescale)
+        check_if_not(None, check_positive, check_int,
+                     n_pca=self.n_pca, self.a)
+        check_if_not('auto', check_positive, check_int,
+                     t=self.t)
+
+    def fit(self, X):
+        """Computes the diffusion operator
+
+        Parameters
+        ----------
+        X : array, shape=[n_samples, n_features]
+            input data with `n_samples` samples and `n_dimensions`
+            dimensions. Accepted data types: `numpy.ndarray`,
+            `scipy.sparse.spmatrix`, `pd.DataFrame`, `anndata.AnnData`. If
+            `knn_dist` is 'precomputed', `data` should be a n_samples x
+            n_samples distance or affinity matrix
+
+        Returns
+        -------
+        magic_operator : MAGIC
+        The estimator object
+        """
+        try:
+            if isinstance(X, anndata.AnnData):
+                X = X.X
+        except NameError:
+            # anndata not installed
+            pass
+        if self.X is not None and not np.all(X == self.X):
+            """
+            If the same data is used, we can reuse existing kernel and
+            diffusion matrices. Otherwise we have to recompute.
+            """
+            self.graph = None
+        self.X = X
+
+        if self.X.shape[1] <= self.n_pca:
+            n_pca = None
+        else:
+            n_pca = self.n_pca
+
+        if self.graph is None:
+
+            log_start("graph and diffusion operator")
+            self.graph = graphtools.Graph(
+                X,
+                n_pca=n_pca,
+                knn=self.k + 1,
+                decay=self.a,
+                thresh=self.alpha_threshold,
+                n_jobs=self.n_jobs,
+                verbose=self.verbose,
+                random_state=self.random_state)
+            log_complete("graph and diffusion operator")
+        else:
+            # check the user hasn't changed parameters manually
+            try:
+                self.graph.set_params(
+                    decay=self.a, knn=self.k + 1,
+                    n_jobs=self.n_jobs, verbose=self.verbose, n_pca=n_pca,
+                    thresh=self.alpha_threshold,
+                    random_state=self.random_state)
+                log_info("Using precomputed graph and diffusion operator...")
+            except ValueError:
+                # something changed that should have invalidated the graph
+                self.graph = None
+                return self.fit(X)
+        return self
+
+    def impute(self, X, t_max=100, plot_optimal_t=False, ax=None):
+        # TODO: landmarks?
+        log_start("imputation")
+        if self.t == 'auto':
+            t = self.optimal_t(X, t_max=t_max, plot=plot_optimal_t, ax=ax)
+            log_info("Automatically selected t = {}".format(t))
+        else:
+            t = self.t
+        if X.shape[1] > self.n_pca:
+            pca = PCA(self.n_pca)
+            X = pca.transform(X)
+
+        for _ in range(t):
+            X = self.diff_op.dot(X)
+
+        if pca is not None:
+            X = pca.inverse_transform(X)
+        log_complete("imputation")
+        return X
+
+    def transform(self, X=None, t_max=100, plot_optimal_t=False, ax=None):
+        """Computes the imputed values
+
+        Parameters
+        ----------
+        X : array, optional, shape=[n_samples, n_features]
+            input data with `n_samples` samples and `n_dimensions`
+            dimensions. By default, we use the data used to call
+            `MAGIC.fit()`.
+            Accepted data types: `numpy.ndarray`,
+            `scipy.sparse.spmatrix`, `pd.DataFrame`, `anndata.AnnData`. If
+            `knn_dist` is 'precomputed', `data` should be a n_samples x
+            n_samples distance or affinity matrix
+
+        t_max : int, optional, default: 100
+            maximum t to test if `t` is set to 'auto'
+
+        plot_optimal_t : boolean, optional, default: False
+            If true and `t` is set to 'auto', plot the Von Neumann
+            entropy used to select t
+
+        ax : matplotlib.axes.Axes, optional
+            If given and `plot_optimal_t` is true, plot will be drawn
+            on the given axis.
+
+        Returns
+        -------
+        embedding : array, shape=[n_samples, n_dimensions]
+        The cells embedded in a lower dimensional space using PHATE
+        """
+        if self.graph is None:
+            raise NotFittedError("This MAGIC instance is not fitted yet. Call "
+                                 "'fit' with appropriate arguments before "
+                                 "using this method.")
+        elif X is not None and not np.all(X == self.X):
+            warnings.warn(UserWarning, "Running MAGIC.transform on different "
+                          "data to that which was used for MAGIC.fit may not "
+                          "produce sensible output.")
+            return self.impute(
+                X, t_max=t_max, plot_optimal_t=plot_optimal_t, ax=ax)
+        else:
+            if self.X_imputed is None:
+                self.X_imputed = self.impute(
+                    self.graph.data_nu, t_max=t_max,
+                    plot_optimal_t=plot_optimal_t, ax=ax)
+                self.X_imputed = self.graph.inverse_transform(self.X_imputed)
+            else:
+                log_info("Using precomputed imputation...")
+            # if isinstance(self.graph, graphtools.LandmarkGraph):
+            #     log_debug("Extending to original data...")
+            #     return self.graph.interpolate(self.X_imputed)
+            return self.X_imputed
+
+    def fit_transform(self, X, **kwargs):
+        """Computes the diffusion operator and the imputed values
+
+        Parameters
+        ----------
+        X : array, shape=[n_samples, n_features]
+            input data with `n_samples` samples and `n_dimensions`
+            dimensions. Accepted data types: `numpy.ndarray`,
+            `scipy.sparse.spmatrix`, `pd.DataFrame`, `anndata.AnnData` If
+            `knn_dist` is 'precomputed', `data` should be a n_samples x
+            n_samples distance or affinity matrix
+
+        kwargs : further arguments for `PHATE.transform()`
+            Keyword arguments as specified in :func:`~phate.PHATE.transform`
+
+        Returns
+        -------
+        embedding : array, shape=[n_samples, n_dimensions]
+            The cells embedded in a lower dimensional space using PHATE
+        """
+        log_start('MAGIC')
+        self.fit(X)
+        X_imputed = self.transform(**kwargs)
+        log_complete('MAGIC')
+        return X_imputed
+
+    def optimal_t(self, data, plot=False,
+                  t_max=20, compute_t_n_genes=500):
+        """Find the optimal value of t
+
+        Selects the optimal value of t based on the R squared of
+        successively higher values of t
+
+        Parameters
+        ----------
+        t_max : int, default: 100
+            Maximum value of t to test
+
+        plot : boolean, default: False
+            If true, plots the Von Neumann Entropy and knee point
+
+        ax : matplotlib.Axes, default: None
+            If plot=True and ax is not None, plots the VNE on the given axis
+            Otherwise, creates a new axis and displays the plot
+
+        Returns
+        -------
+        t_opt : int
+            The optimal value of t
+        """
+        log_start("optimal t")
+        t_opt, r2_vec = compute_optimal_t(data, self.diff_op,
+                                          t_max=t_max,
+                                          compute_t_n_genes=compute_t_n_genes)
+        log_complete("optimal t")
+
+        if plot:
+            plt.figure()
+            plt.plot(np.arange(1, t_max + 1), r2_vec)
+            plt.plot(t_opt, r2_vec[t_opt - 1], 'ro', markersize=10,)
+            plt.plot(np.arange(1, t_max + 1),
+                     np.full(len(r2_vec), 0.05), 'k--')
+            plt.xlabel('t')
+            plt.ylabel('1 - R^2(data_{t},data_{t-1})')
+            plt.xlim([1, t_max])
+            plt.ylim([0, 1])
+            plt.tight_layout()
+    #     legend({'y' 'optimal t' 'y=0.05'});
+
+        return t_opt
---
+++ python/magic/optimal_t.py
@@ -0,0 +1,80 @@
+import numpy as np
+from scipy import sparse
+
+
+def compute_optimal_t(data, diff_op, make_plots=True, t_max=32, n_genes=None):
+
+    if n_genes is None:
+        n_genes = data.shape[1]
+
+    if not sparse.issparse(diff_op):
+        diff_op = sparse.csr_matrix(diff_op)
+
+    if n_genes < data.shape[1]:
+        idx_genes = np.random.randint(data.shape[1], size=n_genes)
+    else:
+        idx_genes = np.arange(data.shape[1])
+    data_imputed = data
+    data_imputed = data_imputed[:, idx_genes]
+
+    if data_imputed.min() < 0:
+        print('data has negative values, shifting to positive')
+        data_imputed = data_imputed - data_imputed.min()
+
+    r2_vec = np.full(t_max, np.nan)
+    data_prev = data_imputed
+    data_prev = np.divide(data_prev, np.broadcast_to(
+        data_prev.sum(axis=0), data_prev.shape))
+
+    complete = False
+    for i in range(t_max):
+        data_imputed = diff_op.dot(data_imputed)
+        data_curr = data_imputed
+        data_curr = np.divide(data_curr, np.broadcast_to(
+            data_curr.sum(axis=0), data_curr.shape))
+        r2, rmse = rsquare(data_prev, data_curr)
+        r2_vec[i] = 1 - r2
+        if r2_vec[i] < 0.05:
+            complete = True
+            t_opt = i + 2
+            break
+        data_prev = data_curr
+
+    if complete:
+        print('optimal t = ', t_opt)
+    else:
+        # r2 has not converged
+        t_opt = t_max
+        print("Warning: optimal t > t_max ({})".format(t_opt))
+
+    return t_opt, r2_vec
+
+
+def rsquare(y, f, c=True):
+
+    if y.shape != f.shape:
+        raise RuntimeError('Y and F must be the same size.')
+
+    # flatten arrays
+    y = np.ravel(y)
+    f = np.ravel(f)
+
+    # remove NaNs
+    temp = np.intersect1d(np.where(np.isfinite(y))[
+                          0], np.where(np.isfinite(f))[0])
+    y = y[temp]
+    f = f[temp]
+
+    if c:
+        r2 = max(0, 1 - np.power(y - f, 2).sum() /
+                 np.power(y - y.mean(), 2).sum())
+    else:
+        r2 = 1 - np.power(y - f, 2).sum() / np.power(y, 2).sum()
+        if r2 < 0:
+            # http://web.maths.unsw.edu.au/~adelle/Garvan/Assays/GoodnessOfFit.html
+            print('Warning: Consider adding a constant term to your model')
+            r2 = 0
+
+    rmse = np.sqrt(np.power((y - f).mean(), 2))
+
+    return r2, rmse
