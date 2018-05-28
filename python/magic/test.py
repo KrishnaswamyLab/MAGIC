46dd361c4f18fdd4c5d5ca0e490f949fc9edff76
Scott Gigante <scottgigante@gmail.com>
2018-05-28 17:50:11 -0400

    initial attempt at refactor

--- python/magic/test.py
+++ python/magic/test.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
-# author: Daniel Burkhardt <daniel.burkhardt@yale.edu>
-# (C) 2017 Krishnaswamy Lab GPLv2
 
 # Generating random fractal tree via DLA
 from __future__ import print_function, division
 import doctest
 
 
-def test_phate():
+def test_magic():
     doctest.testmod()
 
 
 if __name__ == "main":
-    test_phate()
+    test_magic()
