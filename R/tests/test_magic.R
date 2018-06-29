57207610cd2cef91f670a7e52ab694998ede568f
Scott Gigante <scottgigante@gmail.com>
2018-06-29 14:59:04 -0400

    update test

--- R/tests/test_magic.R
+++ R/tests/test_magic.R
@@ -1,24 +1,26 @@
 # To run this file:
 # - Set the working directory to 'R/tests'.
-# - Unzip the data files in 'data'.
 
-source('../R/run_magic.R')
+library(Rmagic)
+library(ggplot2)
+library(readr)
+library(viridis)
 
 test_magic <- function() {
-  require(ggplot2)
+  # load data
+  data <- read.csv('../../data/HMLE_TGFb_day_8_10.csv.gz')
 
-	# load data
-	data <- read.csv('../../data/HMLE_TGFb_day_8_10.csv', header=TRUE, sep=',')
+  # run MAGIC
+  data_MAGIC <- magic(data)
 
-	# run MAGIC
-	data_MAGIC <- run_magic(data, rescale_percent=0.99)
+  # plot
+  p <- ggplot(data) +
+         geom_point(aes(VIM, CDH1, colour=ZEB1)) +
+         scale_colour_viridis(option="B")
+  ggsave('EMT_data_R_before_magic.png', plot=p, width=5, height=5)
 
-	# plot
-	p <- ggplot(data)
-	p + geom_point(aes(VIM, CDH1, colour=ZEB1)) + scale_colour_gradient(low = 'purple', high='yellow')
-  ggsave('EMT_data_R_before_magic.png', width=5, height=5)
-
-  p_m <- ggplot(data_MAGIC)
-  p_m + geom_point(aes(VIM, CDH1, colour=ZEB1)) + scale_colour_gradient(low = 'purple', high='yellow')
-  ggsave('EMT_data_R_after_magic.png', width=5, height=5)
+  p_m <- ggplot(data_MAGIC) +
+           geom_point(aes(VIM, CDH1, colour=ZEB1)) +
+           scale_colour_viridis(option="B")
+  ggsave('EMT_data_R_after_magic.png', plot=p_m, width=5, height=5)
 }
