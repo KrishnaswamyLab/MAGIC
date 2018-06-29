646689635a54e5e8706bb7561306835e81500b8e
Scott Gigante <scottgigante@gmail.com>
2018-06-29 19:25:28 -0400

    test python2 and python3

--- travis_setup.sh
+++ travis_setup.sh
@@ -2,14 +2,23 @@
 
 # install python
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda2.sh
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3.sh
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda2.sh
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda3.sh
 fi
 
-bash miniconda.sh -b -p $HOME/miniconda
+bash miniconda2.sh -b -p $HOME/miniconda2
 hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-pip install --upgrade pip
+$HOME/miniconda2/conda config --set always_yes yes --set changeps1 no
+$HOME/miniconda2/conda update -q conda
+$HOME/miniconda2/conda info -a
+$HOME/miniconda2/pip install --upgrade pip
+
+bash miniconda3.sh -b -p $HOME/miniconda3
+hash -r
+$HOME/miniconda3/conda config --set always_yes yes --set changeps1 no
+$HOME/miniconda3/conda update -q conda
+$HOME/miniconda3/conda info -a
+$HOME/miniconda3/pip install --upgrade pip
