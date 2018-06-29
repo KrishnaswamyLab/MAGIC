d4cac82d006403e7e040b45159e5a715fee12210
Scott Gigante <scottgigante@gmail.com>
2018-06-29 13:56:24 -0400

    add is check in matrix equivalence

--- python/magic/utils.py
+++ python/magic/utils.py
@@ -103,8 +103,8 @@ def matrix_is_equivalent(X, Y):
     """
     Checks matrix equivalence with numpy, scipy and pandas
     """
-    return isinstance(X, Y.__class__) and X.shape == Y.shape and \
-        np.sum((X != Y).sum()) == 0
+    return X is Y or (isinstance(X, Y.__class__) and X.shape == Y.shape and
+                      np.sum((X != Y).sum()) == 0)
 
 
 def convert_to_same_format(data, target_data, columns=None):
