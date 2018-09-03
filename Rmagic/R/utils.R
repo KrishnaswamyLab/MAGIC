62c84732de738c835cb0b9efab1ae9b210965bf6
Scott Gigante <scottgigante@gmail.com>
2018-09-03 12:04:42 -0400

    fix grep length zero

--- Rmagic/R/utils.R
+++ Rmagic/R/utils.R
@@ -19,7 +19,7 @@ load_pymagic <- function(delay_load = FALSE) {
       (length(grep("ModuleNotFoundError: No module named 'magic'", result)) > 0 ||
         length(grep("ImportError: No module named magic", result)) > 0)) {
     install.magic()
-  } else if (grep("r\\-reticulate", reticulate::py_config()$python)) {
+  } else if (length(grep("r\\-reticulate", reticulate::py_config()$python)) > 0) {
     message("Consider removing the 'r-reticulate' environment by running:")
     if (grep("virtualenvs", reticulate::py_config()$python)) {
       message("reticulate::virtualenv_remove('r-reticulate')")
